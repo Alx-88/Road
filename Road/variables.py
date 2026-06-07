0b3de64e958da68b5adbfc7d2d71144c7e3e3304
Alx-88 <98451435+Alx-88@users.noreply.github.com>
2026-06-07 16:49:51 +0200

    Add variables.py with icons_path

---
+++ Road/variables.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+"""Variables for Road workbench."""
+
+import os
+
+# Path to icons
+icons_path = os.path.join(os.path.dirname(__file__), "..", "freecad", "road", "resources")
