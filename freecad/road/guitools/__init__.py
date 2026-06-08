35c70e49ab300ea3e58bd6d65fa6f6012b006712
Alx-88 <98451435+Alx-88@users.noreply.github.com>
2026-06-08 08:20:01 +0200

    Update guitools __init__.py to import working_plane

--- freecad/road/guitools/__init__.py
+++ freecad/road/guitools/__init__.py
@@ -1,3 +1,34 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 """Modules that define the workbench GuiCommands to perform graphical actions."""
+
+from . import (
+    gui_alignment_create,
+    gui_alignment_edit,
+    gui_alignment_object,
+    gui_alignment_offset,
+    gui_component_add,
+    gui_component_line,
+    gui_component_point,
+    gui_component_shape,
+    gui_geopoints_add,
+    gui_geopoints_create,
+    gui_geopoints_export,
+    gui_geopoints_import,
+    gui_landxml_import,
+    gui_pad,
+    gui_profile_add,
+    gui_profile_create,
+    gui_profile_edit,
+    gui_profile_frame_create,
+    gui_region_create,
+    gui_road_create,
+    gui_sections,
+    gui_structure,
+    gui_table,
+    gui_terrain_create,
+    gui_terrain_demolish,
+    gui_terrain_edit,
+    gui_terrain_object,
+    gui_volume,
+    gui_working_plane)
